dcf5cd212e006e8d463854fa4817c20fc4e672fe
Alexsander Leal <alexsander.leal86@gmail.com>
2022-11-21 14:47:30 +0000

    Aula 12 concluida

---
+++ Aula012/ex013.js
@@ -0,0 +1,18 @@
+/* Como pegar a hora atual*/
+var agora = new Date()
+
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+
+if (hora >= 0 && hora <=5) {
+    console.log(`Boa madrugada!`)
+} 
+    else if (hora >= 6 && hora <=11) {
+        console.log(`Bom dia!`)
+    } 
+        else if (hora >= 12 && hora <= 17) {
+            console.log(`Boa tarde!`)
+        }           
+            else {
+                console.log(`Boa noite!`)
+            }
