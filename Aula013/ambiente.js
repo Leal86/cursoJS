e1bacb28e8604179c3dbd42aee4246577b5b4607
Alexsander Leal <alexsander.leal86@gmail.com>
2022-12-03 12:30:10 +0000

    Aula 13 - Estruturas de repetição

---
+++ Aula013/ambiente.js
@@ -0,0 +1,15 @@
+/* Estrutura de repetição WHILE 
+
+var c = 0
+while(c < 501) {
+    console.log(`${c} Tido`)
+    c++
+} */
+
+/* Estrutura de repetição DO WHILE */
+
+var c = 0
+do {
+    console.log(`${c} Tido`)
+    c++
+} while(c < 5)
