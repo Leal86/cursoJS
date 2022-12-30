68c6d28e7627a135d99306a32177f51836598ffc
Alexsander Leal <alexsander.leal86@gmail.com>
2022-12-30 12:54:57 +0000

    Conclui o exercício 17 da aula 14

--- Aula014ex/ex017/script.js
+++ Aula014ex/ex017/script.js
@@ -1,2 +1,22 @@
-window.alert(`Olá!`)
-window.document('ola')
+function tabuada() {
+    var numero = document.getElementById('inumber')
+    var tab = document.getElementById('seltab')    
+    var res = document.getElementById('res')
+    
+    if (numero.value.length == 0){
+        window.alert('[ERRO]: Por favor digite um número! ')
+
+    } else{
+        var n = Number(numero.value)
+        tab.innerHTML = ''
+
+        for (var c = 0; c <= 10 ; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+
+    
+}
