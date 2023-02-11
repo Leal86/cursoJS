d93393891d8976032341bb728352ade5fd0a3496
Alexsander Leal <alexsander.leal86@gmail.com>
2023-02-11 14:11:43 +0000

    Terminei a aula15 do modulo f

---
+++ Aula015/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5,8,2,9,3]
+num[6] = 1
+num.push(4)
+
+console.log(`Nosso vetor é o ${num}.`)
+
+console.log(`O vetor possui ${num.length} posições.`)
+
+console.log(`O vetor organizado em ordem crescente é ${num.sort()}`)
+
+let pos = num.indexOf(10)
+
+if(pos == -1) {
+    console.log(`O valor pesquisado não existe nesse vetor`)
+} else {
+    console.log(`O valor 8 esta na posição ${pos}`)
+}
